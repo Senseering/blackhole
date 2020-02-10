0d039cbbc6d07fb489bc94a96302162159920c66
Felix Mönckemeyer <felix.moenckemeyer@rwth-aachen.de>
2020-02-10 17:28:26 +0100

    stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Healthy');
+});
+
+
+app.listen(80, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+var app2 = express();
+
+app2.get('/', function (req, res) {
+    res.send('Healthy');
+});
+
+
+app2.listen(443, function () {
+    console.log('Example app listening on port 3000!');
+});
